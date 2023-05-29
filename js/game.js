58255490161346bc9ecfd1493e8f12a37effe29a
withercraft303 <lsntao.withercraft303@gmail.com>
2023-05-29 12:35:29 -0400

    guessing game fixes

--- js/game.js
+++ js/game.js
@@ -1,13 +1,11 @@
-
-
 var score = 0
 var qIndex = 0;
 var questions = [];
-let maxQ = 10;
+let maxQ = 5-1;
 
-function setup() {
+async function setup() {
   
-  fetch("https://withercraft303.github.io/hearing_loss/js/questions.txt")
+  await fetch("https://withercraft303.github.io/hearing_loss/js/questions.txt")
     .then(res => res.json())
     .then((data) => {
       console.log(data);
@@ -16,43 +14,56 @@ function setup() {
     console.log("no file");
     console.error(err);
   });
-  var gameObjs = document.getElementsByClassName("game");
-  for (var i = 0; i < gameObjs.length; i++){
-    gameObjs[i].style.visibility = "visible";
+  document.getElementById("pregame").style.visibility = "hidden";
+  document.getElementById("game-display").style.visibility = "visible";
+  document.getElementById("game-controls").style.visibility = "visible";
+  
+  document.getElementById("display-object").innerHTML = questions[qIndex]["object"];
+  document.getElementById("display-score").innerHTML = "Score: " + score 
+}
+
+function shuffle(){
+  var indecies = Set();
+
+  for (var i = 0; i < maxQ; i++) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
   }
-  var pregameObjs = document.getElementsByClassName("pregame");
-  for (var i = 0; i < pregameObjs.length; i++){
-    pregameObjs[i].style.visibility = "hidden";
-  }
-  document.getElementById("display").innerHTML = "Score: " + score + "\n" + questions[qIndex];
 }
 
 function isSafe() {
   if (questions[qIndex]["db"] < 70){
     score += 1;
   }
-  if (qIndex < 10){
-    qIndex += 1;
-  }
-  document.getElementById("display").innerHTML = "Score: " + score + "\n" + questions[qIndex];
+  loadNextQuestion(questions[qIndex]["db"] < 70);
 }
-
 function isUnsafe() {
   if (70 <= questions[qIndex]["db"] && questions[qIndex]["db"] <= 120){
     score += 1;
   }
-  if (qIndex < 10){
-    qIndex += 1;
-  }
-  document.getElementById("display").innerHTML = "Score: " + score + "\n" + questions[qIndex];
+  loadNextQuestion(70 <= questions[qIndex]["db"] && questions[qIndex]["db"] <= 120);
 }
-
 function isDangerous() {
   if (120 < questions[qIndex]["db"]){
     score += 1;
   }
-  if (qIndex < 10){
+  loadNextQuestion(120 < questions[qIndex]["db"]);
+}
+
+function loadNextQuestion(correct){
+  /*setInterval(function() {console.log("a")}, 1000);*/
+  document.getElementById("display-answer").style.visibility = "visible";
+  document.getElementById("display-answer").value = (questions[qIndex]["db"]/150)*100;
+
+  if (qIndex < maxQ){
     qIndex += 1;
+    document.getElementById("display-object").innerHTML = questions[qIndex]["object"];
+    document.getElementById("display-score").innerHTML = "Score: " + score 
+  } else {
+    document.getElementById("display-object").innerHTML = "You finished!";
+    document.getElementById("display-score").innerHTML = "Your final score:"  + score + "/" + (maxQ+1)
+    document.getElementById("safe").disabled = true;
+    document.getElementById("unsafe").disabled = true;
+    document.getElementById("dangerous").disabled = true;
+    document.getElementById("display-next").style.visibility = "visible";
   }
-  document.getElementById("display").innerHTML = "Score: " + score + "\n" + questions[qIndex];
 }
