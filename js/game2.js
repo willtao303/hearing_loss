3b83406e89dac209ea2f6fbd1e4ac5166d4cd7c5
withercraft303 <lsntao.withercraft303@gmail.com>
2023-06-04 20:02:55 -0400

    game 2 fin!

--- js/game2.js
+++ js/game2.js
@@ -1,5 +1,4 @@
 var questions;
-var round = 0;
 var index = 0;
 var left;
 var right;
@@ -27,6 +26,27 @@ async function loadgame(){
   });
 
   questions = shuffle(rawData);
+  questions = [{
+    "place": "Physics classroom",
+    "runtime": 61,
+    "dba": 66.0,
+    "db": 39.4,
+    "dose": 1.2
+  },
+  {
+    "place": "Whiteboard tables",
+    "runtime": 49,
+    "dba": 58.5,
+    "db": 30.8,
+    "dose": 0.2
+  },
+  {
+    "place": "Gym",
+    "runtime": 62,
+    "dba": 67.5,
+    "db": 40.9,
+    "dose": 1.8
+  }]
   
   document.getElementById("game-display").classList.add("fade-in");
   document.getElementById("game-options").classList.add("fade-in");
@@ -70,24 +90,24 @@ function rightbutton(){
 function loadNextQuestion(){
   /* general updates */
   document.getElementById("display-score").innerHTML = "Score: " + score;
-  document.getElementById("left-button").innerHTML = questions[left]["place"] + "<br>sound intensity: " + questions[left]["dba"] + "dba";
-  document.getElementById("right-button").innerHTML = questions[right]["place"] + "<br>sound intensity: " + questions[right]["dba"] + "dba";
+  document.getElementById("left-button").innerHTML = questions[left]["place"] + "<br>Sound Intensity: " + questions[left]["dba"] + "dba";
+  document.getElementById("right-button").innerHTML = questions[right]["place"] + "<br>Sound Intensity: " + questions[right]["dba"] + "dba";
 
   document.getElementById("left-button").disabled = true;
   document.getElementById("right-button").disabled = true;
 
 
   if (questions[right]["dba"] > questions[left]["dba"]){
-    document.getElementById("left-button").style.background = "#ff849f";
-    document.getElementById("right-button").style.background = "#a1eeb8";
+    document.getElementById("left-button").classList.add("wrong");
+    document.getElementById("right-button").classList.add("correct");
     if (index < questions.length){
       document.getElementById("left-button").classList.add("change-option");
     }
     left = index;
     index++;
   } else {
-    document.getElementById("left-button").style.background = "#a1eeb8";
-    document.getElementById("right-button").style.background = "#ff849f";
+    document.getElementById("left-button").classList.add("correct");
+    document.getElementById("right-button").classList.add("wrong");
     if (index < questions.length){
       document.getElementById("right-button").classList.add("change-option");
     }
@@ -96,14 +116,19 @@ function loadNextQuestion(){
   }
 
   if (index > questions.length){
+    finish();
     return;
   }
+
   /* change to next question */
   setTimeout(function(){
+    console.log("reset");
     document.getElementById("left-button").innerHTML = questions[left]["place"];
     document.getElementById("right-button").innerHTML = questions[right]["place"];
-    document.getElementById("left-button").style.background = "aliceblue";
-    document.getElementById("right-button").style.background = "aliceblue";
+    document.getElementById("left-button").classList.remove("correct");
+    document.getElementById("left-button").classList.remove("wrong");
+    document.getElementById("right-button").classList.remove("correct");
+    document.getElementById("right-button").classList.remove("wrong");
   }, 1200);
   setTimeout(function(){
     document.getElementById("left-button").disabled = false;
@@ -111,4 +136,26 @@ function loadNextQuestion(){
     document.getElementById("left-button").classList.remove("change-option");
     document.getElementById("right-button").classList.remove("change-option");
   }, 2000);
+}
+
+function finish(){
+  document.getElementById("left-button").classList.add("fade-out");
+  document.getElementById("right-button").classList.add("fade-out");
+  document.getElementById("game-display").classList.add("finish-box-change");
+  document.getElementById("game-text-container").classList.add("finish-text-change");
+  document.getElementById("game-text-container").classList.add("finish-text-change");
+
+  if (score <= 6){
+    document.getElementById("display-line").innerHTML = "Nice!";
+  }
+  if (score == 7){
+    document.getElementById("display-line").innerHTML = "So Close!";
+  }
+  if (score == 8){
+    document.getElementById("display-line").innerHTML = "Perfect!";
+  }
+
+  document.getElementById("display-score").innerHTML = "Score: " + score + "/8";
+  document.getElementById("next-button-bar").classList.remove("hide");
+
 }
